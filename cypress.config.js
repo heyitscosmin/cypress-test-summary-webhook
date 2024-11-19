11cf0e75a37e22f73231c890096b114568a0c782
Cosmin Balauta <b.cosmin96@gmail.com>
2024-11-19 15:22:38 +0200

    added dependencies

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    reporter: 'cypress-multi-reporters',
+    reporterOptions: {
+      reporterEnabled: 'spec, mocha-junit-reporter',
+      mochaJunitReporterReporterOptions: {
+        mochaFile: './test-results/[suiteFilename].xml',
+      },
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
